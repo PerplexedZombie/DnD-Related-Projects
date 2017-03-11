f684cafc9c01266031e98def65cb201a02ef095f
PerplexedZombie <t.ma.dullaghan@gmail.com>
2017-03-11 17:08:44 +0000

    Add files via upload

--- Settings.py
+++ Settings.py
@@ -1,10 +1,13 @@
 #Settings will hold all Globals
-def init():
-    global RaceList
-    global ClassList
+def init(): #This works, somehow...
+    global RaceList #Essentially a list of all the things in this
+    global ClassList#Also functionality
     global UserRace
     global UserClass
-    RaceList = ['Dwarf (Hill)', 'Dwarf (Mountain)', 'Elf (High)', 'Elf (Forest)', \
+    global stats
+
+    
+    RaceList = ['Dwarf Hill', 'Dwarf (Mountain)', 'Elf (High)', 'Elf (Forest)', \
             'Halfling (Lightfoot)', 'Halfling (Stout)', 'Human', 'Dragonborn', \
             'Gnome (Forest)', 'Gnome (Rock)', 'Half-Elf', 'Half-Orc', 'Tiefling']
 
@@ -13,4 +16,7 @@ def init():
              'Wizard']
     UserRace = ''
     UserClass = ''
+    
+    stats = {'Str' : 0, 'Dex' : 0, 'Con' : 0, 'Int' : 0, 'Wis' : 0, 'Cha' : 0}
+
     
